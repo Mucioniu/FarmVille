70ede19d2247973fb0a37856281f01ab9bb1826c
Andrei Mucioniu <58275395+Mucioniu@users.noreply.github.com>
2021-03-12 19:22:30 +0200

    Add files via upload

---
+++ CsLoginRegister/Adauga_categorie.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace CsLoginRegister
+{
+    partial class Adauga_categorie
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.increngatura = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Regn = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.denPopulara = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.denStiintifica = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.calitateSol = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.clasa = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.calitateSol);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.clasa);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Controls.Add(this.saveButton);
+            this.panel1.Controls.Add(this.increngatura);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.Regn);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.denPopulara);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.denStiintifica);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(8, 8);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(758, 391);
+            this.panel1.TabIndex = 0;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.closeButton.Location = new System.Drawing.Point(504, 334);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(99, 36);
+            this.closeButton.TabIndex = 24;
+            this.closeButton.Text = "Inchide";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.saveButton.Location = new System.Drawing.Point(622, 334);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(99, 36);
+            this.saveButton.TabIndex = 23;
+            this.saveButton.Text = "Salveaza";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // increngatura
+            // 
+            this.increngatura.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.increngatura.Location = new System.Drawing.Point(216, 202);
+            this.increngatura.Name = "increngatura";
+            this.increngatura.Size = new System.Drawing.Size(471, 27);
+            this.increngatura.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label5.Location = new System.Drawing.Point(43, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 19);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Increngatura";
+            // 
+            // Regn
+            // 
+            this.Regn.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Regn.Location = new System.Drawing.Point(216, 165);
+            this.Regn.Name = "Regn";
+            this.Regn.Size = new System.Drawing.Size(471, 27);
+            this.Regn.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label4.Location = new System.Drawing.Point(47, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 19);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Regn";
+            // 
+            // denPopulara
+            // 
+            this.denPopulara.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.denPopulara.Location = new System.Drawing.Point(216, 122);
+            this.denPopulara.Name = "denPopulara";
+            this.denPopulara.Size = new System.Drawing.Size(471, 27);
+            this.denPopulara.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label3.Location = new System.Drawing.Point(43, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 19);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Denumire populara";
+            // 
+            // denStiintifica
+            // 
+            this.denStiintifica.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.denStiintifica.Location = new System.Drawing.Point(216, 86);
+            this.denStiintifica.Name = "denStiintifica";
+            this.denStiintifica.Size = new System.Drawing.Size(471, 27);
+            this.denStiintifica.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label2.Location = new System.Drawing.Point(43, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 19);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Denumire stiintifica";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label1.Location = new System.Drawing.Point(271, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 19);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Adauga o noua categorie";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // calitateSol
+            // 
+            this.calitateSol.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.calitateSol.Location = new System.Drawing.Point(216, 279);
+            this.calitateSol.Name = "calitateSol";
+            this.calitateSol.Size = new System.Drawing.Size(471, 27);
+            this.calitateSol.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label6.Location = new System.Drawing.Point(43, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 19);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "CalitateSol";
+            // 
+            // clasa
+            // 
+            this.clasa.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.clasa.Location = new System.Drawing.Point(216, 242);
+            this.clasa.Name = "clasa";
+            this.clasa.Size = new System.Drawing.Size(471, 27);
+            this.clasa.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.label7.Location = new System.Drawing.Point(43, 246);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 19);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Clasa";
+            // 
+            // Adauga_categorie
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+            this.ClientSize = new System.Drawing.Size(774, 407);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Adauga_categorie";
+            this.Padding = new System.Windows.Forms.Padding(8);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adauga_categorie";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox calitateSol;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox clasa;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox increngatura;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox Regn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox denPopulara;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox denStiintifica;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ CsLoginRegister/adaugaComanda.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SLRDbConnector;
+
+namespace CsLoginRegister
+{
+    public partial class adaugaComanda : Form
+    {
+        DbConnector db;
+        public adaugaComanda()
+        {
+            InitializeComponent();
+            db = new DbConnector();
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (isFormValid())
+            {
+                insertValues();
+            }
+        }
+
+        private void insertValues()
+        {
+            DialogResult dialogResult = MessageBox.Show("Sunteti sigur ca doriti sa adaugati noua comanda?", "Confirmare", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dialogResult == DialogResult.Yes)
+            {
+                db.performCRUD("insert into Comenzi ([idFurnizor], [idUtilizator], [Sosire], [Stare]) Values('" + idFurnizor.Text + "','" + idUtilizator.Text + "','" + sosire.Text + "','" + stare.Text + "')");
+                MessageBox.Show("Comanda adaugata cu succes", "Succes", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Dispose();
+            }
+        }
+
+        private bool isFormValid()
+        {
+            if (idFurnizor.Text.Trim() == string.Empty
+                 || idUtilizator.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Va rugam completati campurile obligatorii, adica idFurnizor si idUtilizator", "Unele campuri sunt goale", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
